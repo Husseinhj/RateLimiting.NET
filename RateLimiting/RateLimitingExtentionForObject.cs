2ddeba56299e13bc02d70d3b2b71a0611c10c422
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2018-02-20 20:24:51 +0330

    Create RateLimiting class with Example project

---
+++ RateLimiting/RateLimitingExtentionForObject.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Threading;
+
+namespace RateLimiting
+{
+    /// <summary>
+    /// Rate Limiting (debounce, throttle) for C# Portable Class Library
+    /// </summary>
+    public static class RateLimitingExtentionForObject
+    {
+        #region Private Properties
+
+        private static Timer _throttleTimerInterval;
+        private static Timer _debounceTimerInterval;
+
+        private static Action<object> _debounceAction;
+        private static object _lastObjectDebounce;
+
+        private static Action<object> _throttleAction;
+        private static object _lastObjectThrottle;
+
+        #endregion
+
+        #region Debounce
+
+        /// <summary>
+        /// Debounce reset timer and after last item recieved give you last item. 
+        /// <exception cref="http://demo.nimius.net/debounce_throttle/">See this example for understanding what is RateLimiting and Debounce</exception>
+        /// </summary>
+        /// <param name="obj">Your object</param>
+        /// <param name="interval">Milisecond interval</param>
+        /// <param name="debounceAction">Called when last item call this method and after interval was finished</param>
+        public static void Debounce(this object obj, int interval, Action<object> debounceAction)
+        {
+            _lastObjectDebounce = obj;
+            _debounceAction = debounceAction;
+
+            _debounceTimerInterval?.Dispose();
+
+            _debounceTimerInterval = new Timer(DebounceTimerIntervalOnTick, obj, interval, interval);
+        }
+
+        /// <summary>
+        /// DispatchTimer tick event for debounce
+        /// </summary>
+        /// <param name="state"></param>
+        private static void DebounceTimerIntervalOnTick(object state)
+        {
+            _debounceTimerInterval?.Dispose();
+
+            if (_debounceTimerInterval != null)
+            {
+                _debounceAction?.Invoke(_lastObjectDebounce);
+            }
+
+            _debounceTimerInterval = null;
+        }
+
+        #endregion
+
+        #region Throttle
+
+        /// <summary>
+        /// Throttle give you last objcet when timer was ticked and invoke throttleAction callback.
+        /// <exception cref="http://demo.nimius.net/debounce_throttle/">See this example for understanding what is RateLimiting and Throttle</exception>
+        /// </summary>
+        /// <param name="obj">Your object</param>
+        /// <param name="interval">Milisecond interval</param>
+        /// <param name="throttleAction">Invoked last object when timer ticked invoked</param>
+        public static void Throttle(this object obj, int interval, Action<object> throttleAction)
+        {
+            _lastObjectThrottle = obj;
+            _throttleAction = throttleAction;
+
+            if (_throttleTimerInterval == null)
+            {
+                _throttleTimerInterval = new Timer(ThrottleTimerIntervalOnTick, obj, interval, interval);
+            }
+        }
+
+        /// <summary>
+        /// DispatchTimer tick event for throttle
+        /// </summary>
+        /// <param name="state"></param>
+        private static void ThrottleTimerIntervalOnTick(object state)
+        {
+            _throttleTimerInterval?.Dispose();
+            _throttleTimerInterval = null;
+
+            if (_lastObjectThrottle != null)
+            {
+                _throttleAction?.Invoke(_lastObjectThrottle);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ RateLimitingExample/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using RateLimiting;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RateLimitingExample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void UIElement_OnPointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            e.Pointer.Debounce(100, DebounceAction);
+            e.Pointer.Throttle(100, ThrottleAction);
+
+            var border = new Border()
+            {
+                Background = new SolidColorBrush(Color.FromArgb(255, 0, 191, 255)),
+                Margin = new Thickness(1),
+                Width = 1,
+                Height = StackPanelRegular.Height
+            };
+
+            StackPanelRegular.Children.Add(border);
+            ThrottleAction(null);
+            DebounceAction(null);
+
+            if (StackPanelRegular.ActualWidth > this.ActualWidth - 400)
+            {
+                StackPanelRegular.Children.Clear();
+                StackPanelDebounce.Children.Clear();
+                StackPanelThrottle.Children.Clear();
+            }
+        }
+
+        private async void ThrottleAction(object o)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                if (o != null && StackPanelThrottle.Children.Count > 0)
+                {
+                    StackPanelThrottle.Children.RemoveAt(StackPanelThrottle.Children.Count - 1);
+                }
+                var border = new Border()
+                {
+                    Background = o == null? new SolidColorBrush(Colors.Transparent) : new SolidColorBrush(Color.FromArgb(255, 255, 219, 147)),
+                    Margin = new Thickness(1),
+                    Width = 1,
+                    Height = StackPanelThrottle.Height
+                };
+
+                StackPanelThrottle.Children.Add(border);
+            });
+
+        }
+
+        private async void DebounceAction(object o)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                if (o != null && StackPanelDebounce.Children.Count > 0)
+                {
+                    StackPanelDebounce.Children.RemoveAt(StackPanelDebounce.Children.Count - 1);
+                }
+                var border = new Border()
+                {
+                    Background = o == null ? new SolidColorBrush(Colors.Transparent) :  new SolidColorBrush(Color.FromArgb(255, 32, 178, 170)),
+                Margin = new Thickness(1),
+                    Width = 1,
+                    Height = StackPanelDebounce.Height
+                };
+
+                StackPanelDebounce.Children.Add(border);
+            });
+        }
+    }
+}
