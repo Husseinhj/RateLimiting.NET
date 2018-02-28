bfcd440f816f6a6196bb0e370516492236c4a3d4
Hussein Habibi Juybari <hussein.juybari@gmail.com>
2018-02-28 10:37:13 +0330

    Add new Core .NET sample for RateLimiting project

---
+++ RateLimitingCoreConsoleExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RateLimitingCoreConsoleExample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
